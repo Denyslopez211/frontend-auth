066def2098893f779eb0f24b6156a4654a397a73
Denyslopez211 <82617865+Denyslopez211@users.noreply.github.com>
2023-08-04 15:28:34 -0400

    Update environments.ts

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  baseUrl: 'https://ec2-18-191-205-245.us-east-2.compute.amazonaws.com:3001/api',
+  baseUrl: 'http://ec2-18-191-205-245.us-east-2.compute.amazonaws.com:3001/api',
 };
