45810afdb33a1091539a79d9fbbe8dcb944ca3ff
Denys Lopez <denyslopez211@gmail.com>
2023-08-07 00:55:57 -0400

    add url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  baseUrl: 'ec2-52-14-212-203.us-east-2.compute.amazonaws.com:3001/api',
+  baseUrl: 'http://ec2-52-14-212-203.us-east-2.compute.amazonaws.com:3001/api',
 };
