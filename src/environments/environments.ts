d798e6337a08d18407d7775eca66602c9aaeb981
Denys Lopez <denyslopez211@gmail.com>
2023-08-06 23:14:48 -0400

    conflict

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,3 @@
 export const environment = {
-<<<<<<< HEAD
-  baseUrl: 'http://localhost:3001/api',
-=======
-  baseUrl: 'http://ec2-18-191-205-245.us-east-2.compute.amazonaws.com:3001/api',
->>>>>>> 066def2098893f779eb0f24b6156a4654a397a73
+  baseUrl: 'ec2-52-14-212-203.us-east-2.compute.amazonaws.com:3001/api',
 };
